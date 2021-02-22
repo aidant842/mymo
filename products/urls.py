56f0944944e4466a4ef398eec221c04dc7c3c249
aidant842 <aidant842@gmail.com>
2021-02-22 12:46:30 +0000

    added product table, filter products by category, product_detail view and created listings app

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products_view, name='products')
+    path('', views.products_view, name='products'),
+    path('<int:product_id>/', views.product_detail, name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 
 def products_view(request):
     """ A view to return the products """
+
+    """ Filter by category """
+
+    selected_category = request.GET.get('category', None)
+
+    if selected_category:
+        products = Product.objects.filter(category__name=selected_category)
+    else:
+        products = Product.objects.filter(category__name='sale')
+
     template = 'products/products.html'
 
-    products = Product.objects.all()
     categories = Category.objects.all()
 
     context = {
         'products': products,
         'categories': categories
     }
+    return render(request, template, context)
+
+
+def product_detail(request, product_id):
+    """ A view to return the form to fill out to create a listing """
+    product = get_object_or_404(Product, pk=product_id)
+
+    template = 'products/product_details.html'
+
+    context = {
+        'product': product,
+    }
+
     return render(request, template, context)
