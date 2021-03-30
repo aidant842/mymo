1a6089f70673f745a6aeb7533111dabd6d95899e
aidant842 <aidant842@gmail.com>
2021-03-30 17:06:57 +0000

    updated debug variable

--- mymo/settings.py
+++ mymo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('MYMO_DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mymoireland.herokuapp.com', 'localhost']
 
