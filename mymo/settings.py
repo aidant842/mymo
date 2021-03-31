0511e2686e43e4f9745381e0498972ba71ed5929
aidant842 <aidant842@gmail.com>
2021-03-31 14:01:55 +0000

    turned off debug

--- mymo/settings.py
+++ mymo/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('MYMO_DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-""" DEBUG = 'DEVELOPMENT' in os.environ """
+""" DEBUG = True """
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mymoireland.herokuapp.com', 'localhost']
 
