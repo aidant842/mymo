68b82e9fdc2e7c454e393c571e77cef3d0fd5f78
aidant842 <aidant842@gmail.com>
2021-04-29 19:18:33 +0000

    changed site_id back to 1

--- mymo/settings.py
+++ mymo/settings.py
@@ -106,7 +106,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SITE_ID = 3
+SITE_ID = 1
 ACCOUNT_EMAIL_VERIFICATION = "none"
 
 if 'DEVELOPMENT' in os.environ:
