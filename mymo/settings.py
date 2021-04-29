65cf351d35044f3430b7d2579fbcc5b78654d86e
aidant842 <aidant842@gmail.com>
2021-04-29 19:35:49 +0000

    swapped around addicons js

--- mymo/settings.py
+++ mymo/settings.py
@@ -106,7 +106,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SITE_ID = 1
+SITE_ID = 3
 ACCOUNT_EMAIL_VERIFICATION = "none"
 
 if 'DEVELOPMENT' in os.environ:
