3a005043b3262d6887ddefecc4456fa846fce04f
aidant842 <aidant842@gmail.com>
2021-05-26 10:40:40 +0000

    changed site_id for push

--- mymo/settings.py
+++ mymo/settings.py
@@ -111,7 +111,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1
-SITE_ID = 3
+""" SITE_ID = 3 """
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
